45ad30e28eacf2878ffd516a22e59363dffd6c5f
max-zorn <40857648+max-zorn@users.noreply.github.com>
2022-07-07 21:07:12 +0300

    add get_token()
    Добавлена функция получения токена для работы с web.

--- bot/handlers/functions.py
+++ bot/handlers/functions.py
@@ -1,10 +1,21 @@
 from time import time
 import requests
 from config import config
-#WEB_URL = "http://127.0.0.1:8000/" text url
 
 
-#Надо переписать модуль полностью, сделать функции рабочими(пока только прототипы), сделать функции с post запросами
+# TODO Надо переписать модуль полностью, сделать функции рабочими(пока только прототипы), сделать функции с post запросами
+
+def get_token() -> str:
+    """
+    Функция возвращает токен из web'a по связке логин + пароль (изначально надо зайти в админку и создать там юзера для Бота).
+    В ответ на запрос придет словарь, содержащий имя бота и токен, который
+    нужно использовать при запросах на другие методы api. Слеш в конце адреса обязателен!!!
+    """
+    data = {'username': config.LOGIN_USERNAME, 'password': config.LOGIN_PASSWORD}
+    url = f'{config.WEB_URL}api/login/'
+    response = requests.post(url=url, data=data)
+    return response.json()["token"]
+
 
 def get_warn(user_id) -> dict:
     url = config.WEB_URL + f"api/block/{user_id}?format=json"
